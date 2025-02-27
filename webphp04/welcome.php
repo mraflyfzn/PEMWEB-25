050264c239041fb366c1722ca5a3c298dff269c7
mraflyfzn <mrafly.fauzan123@gmail.com>
2025-02-27 19:33:45 +0700

    first commit

---
+++ webphp04/welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemweb</title>
+</head>
+<body>
+    <h3>Selamat datang</h3>
+    <?php
+    $_nama = "Muhammad Rafly Fauzan";
+    $_umur = "20";
+    $_prodi = "Sistem Informasi";
+    $_kampus = "stt nf";
+    echo "hello";
+    ?>
+    <hr>
+    <?php
+    echo "nama saya adalah $_nama, saya berumur $_umur tahun";
+    echo "<br>"
+</body>
+</html>
